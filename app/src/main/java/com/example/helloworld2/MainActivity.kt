0047a0e22a6685c0f723fb9e13effaf78f1655b5
lhjhen <henrikas.laurisonis@knf.stud.vu.lt>
2025-09-30 18:18:47 +0300

    added button andd text

--- app/src/main/java/com/example/helloworld2/MainActivity.kt
+++ app/src/main/java/com/example/helloworld2/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.helloworld2
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = "Button clicked!"
         }
     }
 }
